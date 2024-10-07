12bd6e13b6d5de6c7aad6c7aaa212c6e201af389
liz-fw <lstebbins@flowwest.com>
2024-10-07 16:25:47 -0700

    tries scaling spawning a lot, but rearing just a little

--- R/build-scenario.R
+++ R/build-scenario.R
@@ -259,9 +259,9 @@ apply_habitat_actions <- function(scenario, params, starting_habitat, starting_h
   if(32 %in% scenario$action) {
     scaling_factor <- 13
     updated_habitat$spawning_habitat = updated_habitat$spawning_habitat * scaling_factor 
-    #updated_habitat$inchannel_habitat_fry = updated_habitat$inchannel_habitat_fry * scaling_factor 
-    #updated_habitat$inchannel_habitat_juvenile = updated_habitat$inchannel_habitat_juvenile * scaling_factor
-    updated_habitat$floodplain_habitat = updated_habitat$floodplain_habitat * scaling_factor
+    updated_habitat$inchannel_habitat_fry = updated_habitat$inchannel_habitat_fry * 2 
+    updated_habitat$inchannel_habitat_juvenile = updated_habitat$inchannel_habitat_juvenile * 2
+    #updated_habitat$floodplain_habitat = updated_habitat$floodplain_habitat * scaling_factor
     #updated_habitat$yolo_habitat = updated_habitat$yolo_habitat * scaling_factor
     #updated_habitat$sutter_habitat = updated_habitat$sutter_habitat * scaling_factor
     #updated_habitat$delta_habitat = updated_habitat$delta_habitat * scaling_factor
