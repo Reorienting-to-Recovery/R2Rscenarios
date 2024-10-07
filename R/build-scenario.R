85489134620b29a0ffd2a6e67ac404ae798c7ecc
liz-fw <lstebbins@flowwest.com>
2024-10-07 16:28:45 -0700

    tries scaling spawning a lot, no rearing scaling, but bypass and delta scaling

--- R/build-scenario.R
+++ R/build-scenario.R
@@ -259,12 +259,12 @@ apply_habitat_actions <- function(scenario, params, starting_habitat, starting_h
   if(32 %in% scenario$action) {
     scaling_factor <- 13
     updated_habitat$spawning_habitat = updated_habitat$spawning_habitat * scaling_factor 
-    updated_habitat$inchannel_habitat_fry = updated_habitat$inchannel_habitat_fry * scaling_factor 
-    updated_habitat$inchannel_habitat_juvenile = updated_habitat$inchannel_habitat_juvenile * scaling_factor
+    #updated_habitat$inchannel_habitat_fry = updated_habitat$inchannel_habitat_fry * scaling_factor 
+    #updated_habitat$inchannel_habitat_juvenile = updated_habitat$inchannel_habitat_juvenile * scaling_factor
     #updated_habitat$floodplain_habitat = updated_habitat$floodplain_habitat * scaling_factor
-    #updated_habitat$yolo_habitat = updated_habitat$yolo_habitat * scaling_factor
-    #updated_habitat$sutter_habitat = updated_habitat$sutter_habitat * scaling_factor
-    #updated_habitat$delta_habitat = updated_habitat$delta_habitat * scaling_factor
+    updated_habitat$yolo_habitat = updated_habitat$yolo_habitat * scaling_factor
+    updated_habitat$sutter_habitat = updated_habitat$sutter_habitat * scaling_factor
+    updated_habitat$delta_habitat = updated_habitat$delta_habitat * scaling_factor
   }
   
   return(updated_habitat)
